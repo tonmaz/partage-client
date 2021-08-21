af071a14bdce505ef7ffb5458a548ee08ef2eef2
Tonmaz <54241333+tonmaz@users.noreply.github.com>
2021-08-21 18:05:05 +0200

    second

--- src/configureAmplify.js
+++ src/configureAmplify.js
@@ -1,3 +1,4 @@
 import Amplify from "aws-amplify";
-import config from "@src/aws-exports";
-Amplify.configure({ ...config, ssr: true });
+import awsconfig from "./aws-config";
+
+Amplify.configure({ ...awsconfig, ssr: true });
